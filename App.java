c8ed1fc736bb91f7ff19dd2e9623dfc27677f720
Tanya <seize.the.day102306@gmail.com>
2025-10-10 18:45:08 +0200

    Set up the window (JFrame)

--- App.java
+++ App.java
@@ -1,3 +1,20 @@
+import javax.swing.JFrame;
+
 public class App {
-    
+    public static void main(String[] args) {
+        // Dimensions of the window
+        int rows = 21;
+        int columns = 19;
+        int tileSize = 32;
+        int boardWidth = columns * tileSize;
+        int boardHight = rows * tileSize;
+
+        //Set up the window
+        JFrame frame = new JFrame("Pac Man");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHight);
+        frame.setResizable(true);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
